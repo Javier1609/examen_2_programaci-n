9139ff8fa7c769d2233e0dcc0f4f66e57363fb9e
Javier1609 <jvrgmz@myuax.com>
2023-12-05 10:56:05 +0100

    examen

--- ejercicio_1.cpp
+++ ejercicio_1.cpp
@@ -5,10 +5,25 @@
 
 class Variante {
 private:
-    enum class Type { INT, FLOAT, STRING, BOOL, NONE };
+    enum class Type {
+        INT, FLOAT, STRING, BOOL, NONE
+    };
 
-    }
+}
 class Environment {
 private:
     std::map<std::string, Variante> symbolTable;
+public:
+
+    Environment() {}
+
+
+    void insert(const std::string& name, const Variante& value) {
+        auto result = symbolTable.insert({name, value});
+        if (!result.second) {
+            throw std::runtime_error("El simbolo ya existe en el entorno.");
+        }
+    }
+
+
 };
